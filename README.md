02d566aad613b3f063ddb70c7d5b11da604b8fe3
Humberto Rocha <humrochagf@gmail.com>
2019-04-18 18:23:11 -0300

    Add resource aware api_root support and change serializer to enable file upload

--- README.md
+++ README.md
@@ -18,6 +18,13 @@ client = Stripe(
 client.charges_list().get()
 ```
 
+### Stripe file creation endpoint
+
+This endpoint has a special base `api_root`.
+
+If you want to use this endpoint make sure your **tapioca-wrapper** version
+is higher than **1.5.1** that has support to [resource aware get_api_root](https://github.com/vintasoftware/tapioca-wrapper/issues/156).
+
 - Learn how Tapioca works [here](http://tapioca-wrapper.readthedocs.org/en/stable/quickstart.html)
 - Explore this package using iPython
 - Have fun!
--- tapioca_stripe/tapioca_stripe.py
+++ tapioca_stripe/tapioca_stripe.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 from requests.auth import HTTPBasicAuth
-from tapioca import (
-    JSONAdapterMixin, TapiocaAdapter, generate_wrapper_from_adapter
-)
+from tapioca import TapiocaAdapter, generate_wrapper_from_adapter
 
 from .resource_mapping import RESOURCE_MAPPING
 
 
-class StripeClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+class FormRequestJSONResponseAdapterMixin(object):
+
+    def format_data_to_request(self, data):
+        return data
+
+    def response_to_native(self, response):
+        if response.content.strip():
+            return response.json()
+
+    def get_error_message(self, data, response=None):
+        if not data and response.content.strip():
+            data = json.loads(response.content.decode('utf-8'))
+
+        if data:
+            return data.get('error', None)
+
+
+class StripeClientAdapter(FormRequestJSONResponseAdapterMixin, TapiocaAdapter):
 
     api_root = 'https://api.stripe.com/v1'
     resource_mapping = RESOURCE_MAPPING
@@ -22,7 +39,11 @@ class StripeClientAdapter(JSONAdapterMixin, TapiocaAdapter):
 
         return params
 
-    def get_api_root(self, api_params):
+    def get_api_root(self, api_params, **kwargs):
+        # stripe file creation specific endpoint
+        if kwargs.get('resource_name') == 'files_create':
+            return 'https://files.stripe.com/v1'
+
         return self.api_root
 
     def get_iterator_list(self, response_data):
