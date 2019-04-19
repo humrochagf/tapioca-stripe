5107f16823c7ea3325468ddd2179f66ddceb81e3
Humberto Rocha <humrochagf@gmail.com>
2019-04-19 01:12:48 -0300

    Add get_api_root test

--- tests/test_tapioca_stripe.py
+++ tests/test_tapioca_stripe.py
@@ -10,6 +10,14 @@ class TestTapiocaStripe(unittest.TestCase):
     def setUp(self):
         self.wrapper = Stripe()
 
+    def test_get_api_root(self):
+        self.assertTrue(
+            self.wrapper.customers_list().data.startswith('https://api.')
+        )
+        self.assertTrue(
+            self.wrapper.files_create().data.startswith('https://files.')
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
